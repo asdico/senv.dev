a480e93f486896a60fbc33d01b706b60983daad4
Lorenzo Demaio <demaio.lorenzo@gmail.com>
2025-11-11 17:09:00 +0100

    feat(repository): implement create_repository function and corresponding frontend page

---
+++ apps/frontend/app/(app)/repository/[id]/page.tsx
@@ -0,0 +1,4 @@
+
+export default function RepositoryPage(){
+    return <div>RepositoryPage</div>
+}
--- apps/frontend/app/(app)/wizard/actions.ts
+++ apps/frontend/app/(app)/wizard/actions.ts
@@ -1,11 +1,22 @@
 'use server'
 
 import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
 
 export async function createRepoAction(formData: FormData){
     const name = formData.get('name') as string;
     console.log(name);
-    await new Promise((resolve) => setTimeout(resolve, 10000));
-
+    
     const supabase = await createClient();
+
+    const {data, error} = await supabase.rpc('create_repository', {
+        project_name: name,
+    });
+    
+    if (error){
+        console.error(error);
+        throw new Error(error.message);
+    }
+
+    redirect(`/repository/${data.id}`);
 }
